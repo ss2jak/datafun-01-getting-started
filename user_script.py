7c1b23e562038ca986d99cd02a441a4256265e55
ss2jak <122465964+ss2jak@users.noreply.github.com>
2023-01-21 23:52:43 -0600

    task4

--- user_script.py
+++ user_script.py
@@ -1,3 +1,82 @@
 """
 Complete the script.
 """
+#Domain:Trinidad
+#Temperature(celcius) determines when we will go to beach this weekend
+
+import math
+
+#Get 3 tenperatures in Celsius from user
+temp1=input("what will the temperature be on friday?")
+temp2=input("what will the temperature be on saturday?")
+temp3=input("what will the temperature be on sunday?")
+
+#Assign each returned string to variable
+temp1=float(temp1)
+temp2=float(temp2)
+temp3=float(temp3)
+temps=[temp1,temp2,temp3]
+
+#sum
+sum=temp1+temp2+temp3
+sum
+
+#average
+average=sum/3  
+average
+
+#smallest(min) and largest(max)
+smallest=min(temps)
+largest=max(temps)
+smallest
+largest
+
+#product
+product=temp1*temp2+temp3
+product
+
+#range
+print('range:',min(temps), '-', max(temps))
+
+#friday temp
+if temp1 < 27:
+    print("No beach today, water won't be warm enough")
+elif temp1 == 27:
+    print("Not sure about beach today")
+else:
+    print("Let's go to the beach!")
+
+#saturday temp
+if temp2 < 27:
+    print("No beach today, water won't be warm enough")
+elif temp2 == 27:
+    print("Not sure about beach today")
+else:
+    print("Let's go to the beach!")
+
+#sunday temp
+if temp3 < 27:
+    print("No beach today, water won't be warm enough")
+elif temp3 == 27:
+    print("Not sure about beach today")
+else:
+    print("Let's go to the beach!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
